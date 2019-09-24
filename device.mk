bb2dcf09b1c83dff09a958173422f25aa06b2a76
Thespartann <danilastefan69@yahoo.ro>
2019-09-24 12:20:55 +0000

    X01BD: Magma bringup
    * Remove lineage specific stuff
    * Update makefiles for magma
    
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- device.mk
+++ device.mk
@@ -257,8 +257,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # HIDL
 PRODUCT_PACKAGES += \
